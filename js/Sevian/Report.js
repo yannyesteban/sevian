d6c49d539ed491f5d0d6601fc885308438479b4f
yannyesteban@hotmail.com <andromeda1024x>
2017-07-25 16:51:28 -0400

    hoy

--- js/Sevian/Report.js
+++ js/Sevian/Report.js
@@ -20,10 +20,13 @@ var Report = false;
 	Page.prototype = {
 		create: function(){
 			
-			this._main = this.target.create("div").addClass("page");
-			this._header = this._main.create("div").addClass("header");
-			this._body = this._main.create("div").addClass("body");
-			this._footer = this._main.create("div").addClass("footer");
+			this._main = this.target.create("div").addClass("main");
+			
+			this._page = this._main.create("div").addClass("page");
+			
+			this._header = this._page.create("div").addClass("header");
+			this._body = this._page.create("div").addClass("body");
+			this._footer = this._page.create("div").addClass("footer");
 			
 			
 		},
@@ -58,13 +61,16 @@ var Report = false;
 	};
 	
 	Report = function(opt){
-		this.height = "400px";
-		this.width = "800px";
+		this.height = "100%";
+		this.width = "100%";
 		
 		this.g = [];
 		this.gIndex = 0;
 		this.p = [];
 		this.pIndex = 0;
+		this._pages = 0;
+		this.headerHTML = "";
+		
 		for(var x in opt){
 			
 			this[x] = opt[x];
@@ -73,29 +79,46 @@ var Report = false;
 		
 		
 		this._main = $(this.main).style({
-			height: this.height,
-			width: this.width,
+			//height: this.height,
+			//width: this.width,
 		});
 		
 		
-		this._header = this._main.create("div").addClass("header");
-		this._body = this._main.create("div").addClass("body");
-		this._foot = this._main.create("div").addClass("foot");
+		//this._header = this._main.create("div").addClass("header");
+		//this._body = this._main.create("div").addClass("body");
+		//this._foot = this._main.create("div").addClass("foot");
 		
 	};
 	
 	
 	Report.prototype = {
 		addPage: function(opt){
+			
+			this._pages++;
+			
 			opt.target = $(this.main);
 			this._page = this.p[this.pIndex++] = new Page(opt);
-			this._page = this._page._body;
 			
+			
+			
+			this._page._header.text(this.headerHTML);
+			if(this._pages === 1){
+				this._page._header.text($("pag1").text(), true);
+			}
+			
+			
+			
+			this._page = this._page._body;
+			this._page.style({
+				width: this.width,
+				height: this.height,
+				
+			});
 			
 			var g = Float.getXY(this._page.get());
-			
+			//db(g.height, "blue")
 			this._page.get().style.maxHeight = g.height+"px";
-			db(g.height)
+			//db(g.height)
 			
 		},
 		
@@ -104,19 +127,35 @@ var Report = false;
 			opt.target = $(this.main);
 			this._page = this.g[this.gIndex++] = new Group(opt);
 			
-			
+		},
+		
+		setHeader: function(html){
+			db(4444)
+			db(html)
+			this.headerHTML = html;
 			
 			
 		},
 		
+		createHtable: function(){
+			var row = this._page.create("div").addClass("row");
+			for(var x in this.fields){
+				cell = row.create("div").addClass("cell").text(this.fields[x].title);
+				
+				
+			}
+			var rxy = Float.getXY(row.get());
+			return  rxy.height;
+			
+		},
+		
 		add: function(data){
-			var l = false, m = false, rxy = false, pxy, row = false, n = 0;
+			var l = false, m = false, rxy = false, pxy, row = false, n = 0, cell=false, x =false;
 			
 			var xy = Float.getXY(this._page.get());
 			
-			db(xy.height, "red")
-			
-			for(var x in data){
+			n = n + this.createHtable();
+			for(x in data){
 				
 				
 				if(x> 0 && x % 10 == 0){
@@ -129,22 +168,34 @@ var Report = false;
 				
 				for(var y in data[x]){
 					 
-					n++;
+					
 					cell = row.create("div").addClass("cell").text(data[x][y]);
 					
 					
 					
 				}
 				
-				rxy = Float.getXY(cell.get());
-				
-				
+				rxy = Float.getXY(row.get());
 				pxy = Float.getXY(this._page.get());
-				if(rxy.height*n>xy.height){
-					n=0;
-					this.addPage({});
-					this._page.append(row);
+				
+				//db(rxy.height, "red");
+				//db(rxy.height*n, "red");
+				//db(pxy.height, "green");
+				
+				n = n + rxy.height;
+				if((n) >pxy.height){
+					n = rxy.height;
 					
+					//this._page.get().style.maxHeight = "auto !important";
+					//this._page.get().style.height = "auto  !important";
+					
+					
+					//this._page.get().style.flex = "0 0 auto";
+					
+					this.addPage({});
+					
+					n = n + this.createHtable();
+					this._page.append(row);
 				}
 				
 				
@@ -165,6 +216,19 @@ var r = new Report({
 	
 	
 });
+
+r.setHeader('<img src="http://localhost/sibo/images/logo_sibo.png">');
+
+r.fields = [
+	{name:"cedula", title:"Cédula de Identidad"},
+	{name:"nombre", title:"Nombre"},
+	{name:"apellido", title:"Apellido"},
+	{name:"edad", title:"Edad"},
+	{name:"ciudad", title:"Ciudad"},
+	
+	
+];
+
 
 r.data=[
 	[
@@ -204,10 +268,14 @@ r.data=[
 	465456,65465465,654654,'sadfsadf','sdf1321'
 ],
 	[
-	465456,65465465,654654,'sadfsadf','sdf1321'
+	465456,"sadfs<br>sda sdadf" ,654654,'sadfs<br>sdasd adf','sdf1321'
 ],
 	[
-	465456,65465465,654654,'sadfsadf','sdf1321'
+	465456,"sadfs<br>sdasd adf",654654,'sadfs<br>sdasd adf','sdf1321'
+],	[
+	465456,"sadfs<br>sdasd adf",654654,'sadfs<br>sdasd adf','sdf1321'
+],	[
+	465456,"sadfs<br>sdasd adf",654654,'sadfs<br>sdasd adf','sdf1321'
 ],
 	[
 	465456,65465465,654654,'sadfsadf','sdf1321'
@@ -300,3 +368,4 @@ r.data=[
 
 r.addPage({});
 r.add(r.data);
+
