2a2d3f3b3ae1669bcd8fde4599a25cb257e62fa4
yannyesteban@hotmail.com <andromeda1024x>
2017-09-20 15:08:12 -0400

    VIT

--- admin/form4.php
+++ admin/form4.php
@@ -3,12 +3,14 @@
 
 class FileUp{
 	
-	public $field = "";
+	public $field = '';
 	public $name = false;
-	public $path = "";
+	public $path = '';
 	public $error = false;
 	
-	public function delete($name=""){
+	public $multiple = true;
+	
+	public function delete($name = ''){
 		
 		if(is_file($this->path.$this->name)){
 			unlink($this->path.$this->name);
@@ -19,60 +21,66 @@ class FileUp{
 		
 	}
 	
-	public function upload($name){
-		
-		foreach ($_FILES["pictures"]["error"] as $key => $error) {
-			if ($error == UPLOAD_ERR_OK) {
-				$tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-				// basename() puede evitar ataques de denegación de sistema de ficheros;
-				// podría ser apropiada más validación/saneamiento del nombre del fichero
-				$name = basename($_FILES["pictures"]["name"][$key]);
-				move_uploaded_file($tmp_name, "$uploads_dir/$name");
-			}
-		}
-	}
 	
-	public function load(){
-		print_r($_FILES[$this->field]);
-		foreach ($_FILES[$this->field]["error"] as $key => $error) {
-			if ($error == UPLOAD_ERR_OK) {
-				$tmp_name = $_FILES[$this->field]["tmp_name"][$key];
-				// basename() puede evitar ataques de denegación de sistema de ficheros;
-				// podría ser apropiada más validación/saneamiento del nombre del fichero
-				$name = basename($_FILES[$this->field]["name"][$key]);
-				echo $name."\n";
-				
-				//move_uploaded_file($tmp_name, "$uploads_dir/$name");
-			}
-		}
-		
-		if($_FILES[$this->field]["name"] == ""){
-			$this->error = true;
-			return false;
-		}
-		
-		$file = $_FILES[$this->field]["name"];
+	private function _load($file, $temp){
 		
 		$info = pathinfo($file);
-		$ext = $info['extension'];
 		
+		$ext = $info['extension'];
+
 		if($this->name){
 			$baseName = $this->name;
 		}else{
-			$baseName = uniqid("f");
+			$baseName = uniqid('f');
 		}
 
-		
+
 		$name = $this->path.$baseName.".".$ext;
-		
-		if(move_uploaded_file($_FILES[$this->field]["tmp_name"], $name)){
+
+		if(move_uploaded_file($temp, $name)){
 			return $name;
-			
+
 		}else{
 			$this->error = true;
 			return false;
 		}
+	}
 	
+	
+	
+	public function load(){
+		
+		if(is_array($_FILES[$this->field]['name'])){
+			
+			$this->multiple = true;
+		}else{
+			$this->multiple = false;
+		}
+		
+		if($this->multiple){
+			
+			$names = array();
+			
+			foreach ($_FILES[$this->field]['error'] as $k => $error) {
+
+				if ($error == UPLOAD_ERR_OK) {
+					$names[] = $this->_load($_FILES[$this->field]['name'][$k], $_FILES[$this->field]['tmp_name'][$k]);
+				}
+
+			}
+			return $names;
+
+		}
+		
+		if($_FILES[$this->field]['error'] == UPLOAD_ERR_OK) {
+
+			
+			return $this->_load($_FILES[$this->field]['name'], $_FILES[$this->field]['tmp_name']);
+			
+
+		}
+		
+		return false;
 		
 	}
 	
@@ -92,10 +100,10 @@ class form4 extends Sevian\Panel{
 		
 		$f = new FileUp();
 		
-		$f->field = "archivo";
-		$f->path = "files/";
+		$f->field = 'archivo';
+		$f->path = 'files/';
 		
-		//$f->name = "esteban";
+		//$f->name = 'esteban';
 		
 		//echo $method;
 		
@@ -110,34 +118,34 @@ class form4 extends Sevian\Panel{
 		
 		
 		
-		$main = new Sevian\HTML("div");
+		$main = new Sevian\HTML('div');
 		
-		$main->class = "xxx";
-		$main->text = "hola";
-		
-		
-		$input = $main->add("input");
-		$input->type = "file";
-		
-		$input->multiple = "multiple";
-		$input->name = "archivo";
+		$main->class = 'xxx';
+		$main->text = 'hola';
 		
 		
-		$input2 = $main->add("input");
-		$input2->type = "button";
+		$input = $main->add('input');
+		$input->type = 'file';
+		
+		$input->multiple = 'multiple';
+		$input->name = 'archivo';
+		
+		
+		$input2 = $main->add('input');
+		$input2->type = 'button';
 		
 		
 		$action = Sevian\Action::Send([
-			"async"=>false,
-			"panel"=>6,
-			"valid"=>false,
-			"params"=>[
+			'async'=>false,
+			'panel'=>6,
+			'valid'=>false,
+			'params'=>[
 			[
-					"setMethod"=>[
-						"panel"=>4,
-						"element"=>"form4",
-						"name"=>"uno",
-						"method"=>"save",
+					'setMethod'=>[
+						'panel'=>4,
+						'element'=>'form4',
+						'name'=>'uno',
+						'method'=>'save',
 					],
 			
 			
@@ -146,11 +154,11 @@ class form4 extends Sevian\Panel{
 				[
 					
 			
-					"setPanel"=>[
-						"panel"=>4,
-						"element"=>"form4",
-						"name"=>"uno",
-						"method"=>"load",
+					'setPanel'=>[
+						'panel'=>4,
+						'element'=>'form4',
+						'name'=>'uno',
+						'method'=>'load',
 					],
 				]
 				
